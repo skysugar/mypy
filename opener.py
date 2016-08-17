8c4358c822150e91c896907aae61c20bf1bbb21b
skysugar <iskysugar@gmail.com>
2016-08-17 22:16:40 +0800

    Update opener.py

--- opener.py
+++ opener.py
@@ -4,9 +4,6 @@
 import urllib.request
 from http import cookiejar
 
-'''
-http://pic.yesky.com/c/6_20491_1.shtml
-'''
 
 def makebrowser():
     UA='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36'
